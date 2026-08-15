98b3ac7a6cd9dc1258505023b77a7c5a5202fcfc
camiar1 <166476715+camiar1@users.noreply.github.com>
2026-08-15 00:57:07 -0500

    Add ingredient dropper

---
+++ BurgerShake/Assets/Scripts/Dropping/IngredientDropper.cs
@@ -0,0 +1,80 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+
+public class IngredientDropper : MonoBehaviour
+{
+    [SerializeField] private Camera gameplayCamera;
+    [SerializeField] private float minX = -4f;
+    [SerializeField] private float maxX = 4f;
+    [SerializeField] private float dropY = 4f;
+
+    private IngredientDefinition selectedIngredient;
+    private IngredientDraftManager draftManager;
+
+    public bool HasIngredient => selectedIngredient != null;
+
+    public void Initialize(IngredientDraftManager manager)
+    {
+        draftManager = manager;
+    }
+
+    public void SetIngredient(IngredientDefinition ingredient)
+    {
+        selectedIngredient = ingredient;
+    }
+
+    private void Awake()
+    {
+        if (gameplayCamera == null)
+        {
+            gameplayCamera = Camera.main;
+        }
+    }
+
+    private void Update()
+    {
+        if (selectedIngredient == null || gameplayCamera == null)
+        {
+            return;
+        }
+
+        Mouse mouse = Mouse.current;
+        if (mouse == null)
+        {
+            return;
+        }
+
+        Vector3 mousePosition = gameplayCamera.ScreenToWorldPoint(mouse.position.ReadValue());
+        transform.position = new Vector3(Mathf.Clamp(mousePosition.x, minX, maxX), dropY, transform.position.z);
+
+        if (mouse.leftButton.wasPressedThisFrame)
+        {
+            if (EventSystem.current != null && EventSystem.current.IsPointerOverGameObject())
+            {
+                return;
+            }
+
+            DropIngredient();
+        }
+    }
+
+    private void DropIngredient()
+    {
+        if (selectedIngredient == null || selectedIngredient.prefab == null)
+        {
+            return;
+        }
+
+        GameObject spawned = Instantiate(selectedIngredient.prefab, transform.position, Quaternion.identity);
+        Ingredient ingredient = spawned.GetComponent<Ingredient>();
+
+        if (ingredient != null)
+        {
+            ingredient.Initialize(selectedIngredient);
+        }
+
+        selectedIngredient = null;
+        draftManager?.IngredientWasDropped();
+    }
+}
