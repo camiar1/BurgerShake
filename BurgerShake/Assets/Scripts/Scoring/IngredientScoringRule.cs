696c3ebb4a873512d6e9f4833608668baeaed4d1
camiar1 <166476715+camiar1@users.noreply.github.com>
2026-08-21 16:47:19 -0500

    Add data-driven ingredient scoring rules

---
+++ BurgerShake/Assets/Scripts/Scoring/IngredientScoringRule.cs
@@ -0,0 +1,66 @@
+using UnityEngine;
+
+public enum ScoringTarget
+{
+    Self,
+    TouchingAny,
+    TouchingTag,
+    TouchingIngredient
+}
+
+public enum ScoringReward
+{
+    Points,
+    Mult
+}
+
+[CreateAssetMenu(fileName = "NewScoringRule", menuName = "Burger Shake/Scoring Rule")]
+public class IngredientScoringRule : ScriptableObject
+{
+    [TextArea] public string description;
+
+    [Header("Condition")]
+    public ScoringTarget target = ScoringTarget.Self;
+    public IngredientTag requiredTag;
+    public IngredientDefinition requiredIngredient;
+
+    [Header("Reward")]
+    public ScoringReward reward = ScoringReward.Points;
+    public float amount = 1f;
+
+    public ScoreValue Evaluate(Ingredient ingredient)
+    {
+        if (ingredient == null)
+        {
+            return default;
+        }
+
+        int triggerCount = GetTriggerCount(ingredient);
+        float total = amount * triggerCount;
+
+        return reward == ScoringReward.Points
+            ? new ScoreValue(Mathf.RoundToInt(total), 0f)
+            : new ScoreValue(0, total);
+    }
+
+    private int GetTriggerCount(Ingredient ingredient)
+    {
+        switch (target)
+        {
+            case ScoringTarget.Self:
+                return 1;
+
+            case ScoringTarget.TouchingAny:
+                return ingredient.TouchingCount;
+
+            case ScoringTarget.TouchingTag:
+                return ingredient.CountTouchingWithTag(requiredTag);
+
+            case ScoringTarget.TouchingIngredient:
+                return ingredient.CountTouchingIngredient(requiredIngredient);
+
+            default:
+                return 0;
+        }
+    }
+}
